5e86b212e6339aabaa8a2e07fdbca71137907dcf
JumabaevPolatbek <mouz.cyx@mail.ru>
2024-01-12 21:24:44 +0500

    footer changed Image/Link

--- src/Components/FooterFAQ/FooterFAQ.tsx
+++ src/Components/FooterFAQ/FooterFAQ.tsx
@@ -1,49 +1,51 @@
 import styles from '../../styles/footer/FooterFAQ.module.scss';
 import { useState } from 'react';
 // import { v4 as uuid } from 'uuid';
+import Image from 'next/image';
+import Link from 'next/link';
 
 function FooterFAQ() {
-	const [active, setActive] = useState(0);
+    const [active, setActive] = useState(0);
 
-	const handleClick = (e: React.MouseEvent) => {
-		const id = (e.target as HTMLSpanElement).id;
-		setActive(+id);
-	};
-	return (
-		<div className={styles['footer-faq']}>
-			<h1 className={'title'}>Вопрос-ответ:</h1>
-			<div className={'questions'}>
-				{/* [1,2,3,4,5] massiviniń ornında maǵlıwmatlar massivi bolıwı kerek */}
+    const handleClick = (e: React.MouseEvent) => {
+        const id = (e.target as HTMLSpanElement).id;
+        setActive(+id);
+    };
+    return (
+        <div className={styles['footer-faq']}>
+            <h1 className={'title'}>Вопрос-ответ:</h1>
+            <div className={'questions'}>
+                {/* [1,2,3,4,5] massiviniń ornında maǵlıwmatlar massivi bolıwı kerek */}
 
-				{[1, 2, 3, 4, 5].map((i: number) => (
-					<div className={'question-answer'} key={i}>
-						<h3
-							className={'question'}
-							id={`${i}`}
-							onClick={handleClick}
-						>
-							<p>Где учиться по удаленной специальности?</p>
-							<span>
-								<img src="/assets/plus.svg" alt="plus" />
-							</span>
-						</h3>
-						<p
-							className={`answer ${
-								active === i ? 'active' : null
-							}`}
-						>
-							Зачеты и экзамены проходят в виде тестов внутри
-							электронного кабинета по расписанию. Тесты открытые,
-							закрытые, с вариантами, иногда нужно решить задачу
-							или написать эссе. Есть три попытки, можно выбрать
-							лучший результат. Время каждой попытки ограничено,
-							обычно от 30 до 45 минут на тест.
-						</p>
-					</div>
-				))}
-			</div>
-		</div>
-	);
+                {[1, 2, 3, 4, 5].map((i: number) => (
+                    <div className={'question-answer'} key={i}>
+                        <h3
+                            className={'question'}
+                            id={`${i}`}
+                            onClick={handleClick}
+                        >
+                            <p>Где учиться по удаленной специальности?</p>
+                            <span>
+                                <Image src='/assets/plus.svg' alt='plus' />
+                            </span>
+                        </h3>
+                        <p
+                            className={`answer ${
+                                active === i ? 'active' : null
+                            }`}
+                        >
+                            Зачеты и экзамены проходят в виде тестов внутри
+                            электронного кабинета по расписанию. Тесты открытые,
+                            закрытые, с вариантами, иногда нужно решить задачу
+                            или написать эссе. Есть три попытки, можно выбрать
+                            лучший результат. Время каждой попытки ограничено,
+                            обычно от 30 до 45 минут на тест.
+                        </p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default FooterFAQ;
--- src/Components/Layouts/Footer/Footer.tsx
+++ src/Components/Layouts/Footer/Footer.tsx
@@ -2,106 +2,107 @@
 import FooterContact from '../../FooterContact';
 import FooterFAQ from '../../FooterFAQ';
 import styles from '../../../styles/footer/Footer.module.scss';
-
+import Image from 'next/image';
+import Link from 'next/link';
 function Footer() {
-	return (
-		<footer className={styles['footer']}>
-			<div className="container">
-				<FooterFAQ />
-				<FooterContact />
-				<div className={'footer__info'}>
-					<div className={'about'}>
-						<img src="./images/logo-footer.svg" alt="logo" />
-						<p className={'description'}>
-							Единая приёмная комиссия вузов и колледжей по
-							дистанционным программам
-						</p>
-						<a href="#">Политика конфиденциальности</a>
-					</div>
-					<ul className={'navs'}>
-						<li>
-							<a href="#">Направления</a>
-						</li>
-						<li>
-							<a href="#">Направления</a>
-						</li>
-						<li>
-							<a href="#">Направления</a>
-						</li>
-						<li>
-							<a href="#">Направления</a>
-						</li>
-						<li>
-							<a href="#">Направления</a>
-						</li>
-						<li>
-							<a href="#">Направления</a>
-						</li>
-						<li>
-							<a href="#">Направления</a>
-						</li>
-						<li>
-							<a href="#">Направления</a>
-						</li>
-						<li>
-							<a href="#">Направления</a>
-						</li>
-						<li>
-							<a href="#">Направления</a>
-						</li>
-						<li>
-							<a href="#">Направления</a>
-						</li>
-						<li>
-							<a href="#">Направления</a>
-						</li>
-					</ul>
-					<div className={'contacts'}>
-						<div className={'contact__info'}>
-							<p className={'phone'}>+7 (902) 311 36 64</p>
-							<p className={'desc__work'}>Рабочее время: </p>
-							<p className={'desc__time'}>
-								{' '}
-								Пн — Пт с 9.00 до 19.00
-							</p>
-						</div>
-						<ul className={'social__links'}>
-							<li>
-								<a href="#" className={'social-link'}>
-									<img
-										src="./images/social/vk.svg"
-										alt="vk"
-									/>
-								</a>
-							</li>
-							<li>
-								<a href="#" className={'social-link'}>
-									<img
-										src="./images/social/instagram.svg"
-										alt="instagram"
-									/>
-								</a>
-							</li>
-							<li>
-								<a href="#" className={'social-link'}>
-									<img
-										src="./images/social/whatsapp.svg"
-										alt="whatsapp"
-									/>
-								</a>
-							</li>
-						</ul>
-					</div>
-					<a href="#" className={'politics-mobile'}>
-						Политика конфиденциальности
-					</a>
-				</div>
-				<div className={'invite'}>
-					<a href="#">Подобрать обучение</a>
-				</div>
-			</div>
-		</footer>
-	);
+    return (
+        <footer className={styles['footer']}>
+            <div className='container'>
+                <FooterFAQ />
+                <FooterContact />
+                <div className={'footer__info'}>
+                    <div className={'about'}>
+                        <Image src='./images/logo-footer.svg' alt='logo' />
+                        <p className={'description'}>
+                            Единая приёмная комиссия вузов и колледжей по
+                            дистанционным программам
+                        </p>
+                        <Link href='#'>Политика конфиденциальности</Link>
+                    </div>
+                    <ul className={'navs'}>
+                        <li>
+                            <Link href='#'>Направления</Link>
+                        </li>
+                        <li>
+                            <Link href='#'>Направления</Link>
+                        </li>
+                        <li>
+                            <Link href='#'>Направления</Link>
+                        </li>
+                        <li>
+                            <Link href='#'>Направления</Link>
+                        </li>
+                        <li>
+                            <Link href='#'>Направления</Link>
+                        </li>
+                        <li>
+                            <Link href='#'>Направления</Link>
+                        </li>
+                        <li>
+                            <Link href='#'>Направления</Link>
+                        </li>
+                        <li>
+                            <Link href='#'>Направления</Link>
+                        </li>
+                        <li>
+                            <Link href='#'>Направления</Link>
+                        </li>
+                        <li>
+                            <Link href='#'>Направления</Link>
+                        </li>
+                        <li>
+                            <Link href='#'>Направления</Link>
+                        </li>
+                        <li>
+                            <Link href='#'>Направления</Link>
+                        </li>
+                    </ul>
+                    <div className={'contacts'}>
+                        <div className={'contact__info'}>
+                            <p className={'phone'}>+7 (902) 311 36 64</p>
+                            <p className={'desc__work'}>Рабочее время: </p>
+                            <p className={'desc__time'}>
+                                {' '}
+                                Пн — Пт с 9.00 до 19.00
+                            </p>
+                        </div>
+                        <ul className={'social__links'}>
+                            <li>
+                                <Link href='#' className={'social-link'}>
+                                    <Image
+                                        src='./images/social/vk.svg'
+                                        alt='vk'
+                                    />
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href='#' className={'social-link'}>
+                                    <Image
+                                        src='./images/social/instagram.svg'
+                                        alt='instagram'
+                                    />
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href='#' className={'social-link'}>
+                                    <Image
+                                        src='./images/social/whatsapp.svg'
+                                        alt='whatsapp'
+                                    />
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <Link href='#' className={'politics-mobile'}>
+                        Политика конфиденциальности
+                    </Link>
+                </div>
+                <div className={'invite'}>
+                    <Link href='#'>Подобрать обучение</Link>
+                </div>
+            </div>
+        </footer>
+    );
 }
 
 export default Footer;
