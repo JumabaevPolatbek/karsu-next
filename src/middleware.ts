da82f6d7de08a94c077311fb13ad7c26c062da1b
JumabaevPolatbek <mouz.cyx@mail.ru>
2024-02-22 19:30:45 +0500

    translations MetaData

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,10 +2,11 @@ import createMiddleware from 'next-intl/middleware';
 
 export default createMiddleware({
 	// A list of all locales that are supported
-	locales: ['en', 'kaa', 'uz'],
+	locales: ['en', 'kaa', 'uz',"ru"],
 
 	// Used when no locale matches
 	defaultLocale: 'kaa',
+	localeDetection:false
 });
 
 export const config = {
